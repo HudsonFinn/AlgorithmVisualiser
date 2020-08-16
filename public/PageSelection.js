4c5eddb0e8c149919e0dbd0ecc7bee698a6dc859
Finn Hudson <11finnh@gmail.com>
2020-08-16 14:18:20 +0100

    feat/CloseBtn

--- public/PageSelection.js
+++ public/PageSelection.js
@@ -13,19 +13,21 @@ something.onclick = function() {
     var infoWindow = document.createElement('div');
     infoWindow.setAttribute('class', 'window');
     var left = document.createElement('div');
+		left.style.borderRight = "0.5px solid grey";
     left.setAttribute('class', 'right')
     var text = document.createElement('h3');
     text.textContent = "Bubble Sort";
 		var creditText = document.createElement('p');
 		creditText.textContent = "Credit: HackerRank youtube channel, https://www.youtube.com/c/HackerrankOfficial/featured"
 		creditText.style.fontSize = "1rem"
-		left.innerHTML = `<iframe style="overflow:hidden;width:90%;height:40%"src="https://www.youtube.com/embed/6Gv8vg0kcHc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+		left.innerHTML = `<iframe style="overflow:hidden;width:100%;height:45%"src="https://www.youtube.com/embed/6Gv8vg0kcHc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
     left.prepend(text);
 		left.appendChild(creditText);
 
 		var closeDiv = document.createElement('div');
+		closeDiv.setAttribute('class', 'exitButton');
     var closeBtn = document.createElement("IMG");
-    closeBtn.setAttribute("src", "start.png");
+    closeBtn.setAttribute("src", "close.PNG");
     closeBtn.setAttribute("width", "40");
     closeBtn.setAttribute("height", "40");
     closeBtn.setAttribute("onclick", "closeWindow()");
@@ -46,14 +48,16 @@ something.onclick = function() {
     prosSubTitle.textContent = "Pros";
     right.appendChild(prosSubTitle);
 		var proContent = document.createElement('p');
-		proContent.textContent = "Very simple to implement and understand";
+		proContent.setAttribute('style', 'white-space: pre;');
+		proContent.textContent = " - Very simple to implement and understand. \r\n";
+		proContent.textContent += " - For already sorted lists it will only make O(n)"
 		proContent.style.fontSize = "1rem"
 		right.appendChild(proContent);
     var consSubTitle = document.createElement('h6');
     consSubTitle.textContent = "Cons";
     right.appendChild(consSubTitle);
 		var conContent = document.createElement('p');
-		conContent.textContent = "Has n^2 complexity";
+		conContent.textContent = "Where n is the number of elements in the list. Bubble sort will compare n² item both in an average case and also in worst case.";
 		conContent.style.fontSize = "1rem"
 		right.appendChild(conContent);
     infoWindow.appendChild(left);
