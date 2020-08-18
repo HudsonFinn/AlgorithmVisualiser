b51fbf33b925341643465b792f578c8bf497334c
Finn Hudson <11finnh@gmail.com>
2020-08-18 01:54:12 +0100

    feat: Created info for bubble, quick and merge sort

--- public/PageSelection.js
+++ public/PageSelection.js
@@ -35,21 +35,50 @@ function cardClick(clicked) {
 		aboutContent.textContent = algos[openWindow].info.about
 		aboutContent.style.fontSize = "1rem"
 		right.appendChild(aboutContent);
-    var prosSubTitle = document.createElement('h6');
-    prosSubTitle.textContent = "Pros";
-    right.appendChild(prosSubTitle);
-		var proContent = document.createElement('p');
-		proContent.setAttribute('style', 'white-space: pre;');
-		proContent.textContent = algos[openWindow].info.pros;
-		proContent.style.fontSize = "1rem"
-		right.appendChild(proContent);
-    var consSubTitle = document.createElement('h6');
-    consSubTitle.textContent = "Cons";
-    right.appendChild(consSubTitle);
-		var conContent = document.createElement('p');
-		conContent.textContent = algos[openWindow].info.cons;
-		conContent.style.fontSize = "1rem"
-		right.append(conContent);
+    var propertiesSubTitle = document.createElement('h6');
+    propertiesSubTitle.textContent = "Properties";
+    right.appendChild(propertiesSubTitle);
+		var properties = algos[openWindow].info.properties
+		var propertiesContentDiv = document.createElement('div');
+		propertiesContentDiv.innerHTML = `<table class="tg">
+			<thead>
+			  <tr>
+			    <th class="tg-7x22">IN-PLACE</th>
+			    <th class="tg-7x22">STABLE</th>
+			    <th class="tg-7x22">BEST</th>
+			    <th class="tg-7x22">AVERAGE</th>
+			    <th class="tg-7x22">WORST</th>
+					<th class="tg-7x22">SPACE</th>
+			  </tr>
+			</thead>
+			<tbody>
+			  <tr>
+			    <td class="tg-r25x">${properties.inPlace}</td>
+			    <td class="tg-r25x">${properties.stable}</td>
+			    <td class="tg-r25x">${properties.best}</td>
+			    <td class="tg-r25x">${properties.average}</td>
+			    <td class="tg-r25x">${properties.worst}</td>
+			    <td class="tg-r25x">${properties.space}</td>
+			  </tr>
+			</tbody>
+			</table>`;
+		right.append(propertiesContentDiv);
+		// var propertiesContent = document.createElement('p');
+		// propertiesContent.setAttribute('style', 'white-space: pre;');
+		// propertiesContent.textContent = algos[openWindow].info.properties;
+		// propertiesContent.style.fontSize = "1rem"
+		// right.appendChild(propertiesContent);
+    var usesSubTitle = document.createElement('h6');
+    usesSubTitle.textContent = "Use case";
+    right.appendChild(usesSubTitle);
+		var usesContent = document.createElement('p');
+		usesContent.textContent = algos[openWindow].info.use;
+		usesContent.style.fontSize = "1rem"
+		right.appendChild(usesContent);
+		var disclaimerContent = document.createElement('p');
+		disclaimerContent.textContent = algos[openWindow].info.disclaimer;
+		disclaimerContent.style.fontSize = "1rem"
+		right.appendChild(disclaimerContent);
 
 		var closeDiv = document.createElement('div');
 		closeDiv.setAttribute('class', 'exitButton');
--- public/algoData.js
+++ public/algoData.js
@@ -6,9 +6,16 @@ var algos = {
 			credit : "https://www.youtube.com/c/HackerrankOfficial/featured"
 		},
 		info : {
-			about : "A comparison based very simple sorting algorithm.",
-			pros : " - Very simple to implement and understand. \r\n - For already sorted lists it will only make O(n)",
-			cons: "Where n is the number of elements in the list. Bubble sort will compare n² item both in an average case and also in worst case."
+			about : "A very simple comparison based sorting algorithm used for education.",
+			properties : {
+				inPlace : "✓",
+				stable : "✓",
+				best : "n",
+				average : "½ n&sup2;",
+				worst : "½ n&sup2;",
+				space : "1"
+			},
+			use : "Has very little real world usage, generally insertion sort should be used instead as it is faster in most cases. It does have uses in teaching algorithms as it is one of the most simple algorithms."
 		}
 	},
 	"MergeSort" : {
@@ -18,9 +25,17 @@ var algos = {
 			credit : "https://www.youtube.com/c/HackerrankOfficial/featured"
 		},
 		info : {
-			about : "A comparison based very simple sorting algorithm.",
-			pros : " - Very simple to implement and understand. \r\n - For already sorted lists it will only make O(n)",
-			cons: "Where n is the number of elements in the list. Bubble sort will compare n² item both in an average case and also in worst case."
+			about : "A general-purpose, divide and conquer, comparison-based sorting algorithm which uses two arrays to split and then merge the main array recursively.",
+			properties : {
+				inPlace : "X",
+				stable : "✓",
+				best : "½ n lg n",
+				average : "n lg n",
+				worst : "n lg n",
+				space : "n"
+			},
+			use : "Has extensive real world use. Should be used when space is not an issue and in-place sorting is not required. It has guaranteed n log n perfomance which is one of the best of all of algorithms.",
+			disclaimer : ""
 		}
 	},
 	"QuickSort" : {
@@ -30,9 +45,18 @@ var algos = {
 			credit : "https://www.youtube.com/c/HackerrankOfficial/featured"
 		},
 		info : {
-			about : "A comparison based very simple sorting algorithm.",
-			pros : " - Very simple to implement and understand. \r\n - For already sorted lists it will only make O(n)",
-			cons: "Where n is the number of elements in the list. Bubble sort will compare n² item both in an average case and also in worst case."
+			about : "A recursive divide and conquer algorithm which works by selecting a pivot and partitioning the array into two subarrays and then comparing and swapping them.",
+			properties : {
+				inPlace : "✓",
+				stable : "X",
+				best : "n lg n",
+				average : "2 n ln n",
+				worst : "½ n 2",
+				space : "log n*"
+
+			},
+			use : "Does not have guaranteed logarithmic performance but in real world use it is the fastest algorithm. Should not be used if stablity or space is an issue.",
+			disclaimer : "*Depends on implementation, more technical versions can be n (in-place partitioning)"
 		}
 	},
 	"HeapSort" : {
